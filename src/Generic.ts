f118d416a7bedcb9ed9e38f1a4eafc0e876626c8
Tarun Rawat <tarunrawat8350@gmail.com>
2026-09-15 15:58:25 +0530

    generics

--- src/Generic.ts
+++ src/Generic.ts
@@ -11,3 +11,23 @@ function pair<A,B>(a:A,b:B):[A,B]{
 return [a,b]
 }
 pair("Tarun",{age:24})
+pair("masala",20)
+pair("rahul",{age:26})
+
+interface Box<T>{
+    content:T
+}
+
+const numberBox:Box<number>={content:10}
+
+interface ApiPromise<T>{
+    stauts:number,
+    data:T
+}
+
+const res : ApiPromise<{flavor:string}> ={
+stauts:200,
+data:{
+    flavor:"masala"
+}
+}
